dacb618aab2fc65dc88e25a9eb5b50f897cce1d2
Faithfulzp <fzp_hangzhou@163.com>
2022-08-06 14:14:02 +0800

    路由懒加载

--- app/src/router/routes.js
+++ app/src/router/routes.js
@@ -1,19 +1,27 @@
 //引入一级路由插件
-import Home from '@/pages/Home';
-import Login from '@/pages/Login';
-import Register from '@/pages/Register';
-import Search from '@/pages/Search';
-import Detail from '@/pages/Detail';
-import AddCartSuccess from '@/pages/AddCartSuccess';
-import ShopCart from '@/pages/ShopCart';
-import Trade from '@/pages/Trade';
-import Pay from '@/pages/Pay';
-import PaySuccess from '@/pages/PaySuccess';
-import Center from '@/pages/Center';
-//引入二级路由插件
-import MyOrder from '@/pages/Center/MyOrder';
-import GroupOrder from '@/pages/Center/GroupOrder';
+/* 
+    路由懒加载：
+        当打包构建应用时，JavaScript 包会变得非常大，影响页面加载。
+        如果我们能把不同路由对应的组件分割成不同的代码块，
+        然后当路由被访问的时候才加载对应组件，这样就会更加高效。
+*/
+// 方法一：箭头函数赋值
+const Home = () => import('@/pages/Home');
+const Login = () => import('@/pages/Login');
+const Register = () => import('@/pages/Register');
+const Search = () => import('@/pages/Search');
+const Detail = () => import('@/pages/Detail');
+const AddCartSuccess = () => import('@/pages/AddCartSuccess');
+const ShopCart = () => import('@/pages/ShopCart');
+const Trade = () => import('@/pages/Trade');
+const Pay = () => import('@/pages/Pay');
+const PaySuccess = () => import('@/pages/PaySuccess');
+const Center = () => import('@/pages/Center');
+// 方法二：直接在路由里，在组件component里引入,但是可能导致路由路径消失
 
+//引入二级路由插件
+const MyOrder = () => import('@/pages/Center/MyOrder');
+const GroupOrder = () => import('@/pages/Center/GroupOrder');
 //创建一个路由实例
 //配置路由,向外暴露路由数组
 export default [
