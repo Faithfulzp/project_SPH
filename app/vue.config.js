3aa23dfaa25b8075968deb1e5b898637b87ef224
Faithfulzp <fzp_hangzhou@163.com>
2022-08-06 14:23:19 +0800

    打包不要.map文件

--- app/vue.config.js
+++ app/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,//关闭语法检查
-
+  productionSourceMap: false,//上线打包时不要.map文件
   devServer: {
     proxy: {
       '/api': {
